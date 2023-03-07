c68ee5878e266120390b4f8c67c999bd94664c40
Bugnar Catalin <catalin.bugnar@yahoo.ro>
2023-03-07 22:17:55 +0200

    commit lab 1

---
+++ Proiect/Proiect/Domain/Rezervare.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect.Domain
+{
+    public class Rezervare : Entity<string>
+    {
+        [Required]
+        public string idExcursie { get; set; }
+
+        [Required]
+        public string idPersoana { get; set; }
+
+        [Required]
+        public int nrBilete { get; set; }
+    }
+}
---
+++ Proiect/Proiect/Repository/Repository.cs
@@ -0,0 +1,18 @@
+﻿using Proiect.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect.Repository
+{
+    public interface Repository<ID,entity> where entity : Entity<ID>
+    {
+        void adauga(entity entity);
+        void sterge(entity entity);
+        entity cautaId(String id);
+        List<entity> getAll();
+        void update(entity entitate, entity nouaEntitate);
+    }
+}
